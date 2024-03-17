f7b04c71c2c3271231827b8f34b0b33ebbc2f0c4
anabexd <anabeatrizm438@gmail.com>
2024-03-17 15:34:37 -0300

    feat: page

---
+++ web/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-row gap-40 items-center  justify-center p-24 bg-neutral-50">
+    <p className="text-3xl font-semibold text-zinc-600 w-1/4">Chegou a hora de <span className="text-orange-600">otimizar</span> o cardápio da cantina.</p>
+    <Image src='/logo.svg' width={200} height={200} alt=""></Image>
+    </main>
+  );
+}
