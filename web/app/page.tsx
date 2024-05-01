cc98d96bf99120b8b33c1a96a26881214e06ba0f
anabexd <anabeatrizm438@gmail.com>
2024-05-01 18:54:27 -0300

    feat: header

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,10 +1,15 @@
+'use client'
 import Image from "next/image";
+import Header from "../components/header";
 
 export default function Home() {
   return (
+    <>
     <main className="flex min-h-screen flex-row gap-40 items-center  justify-center p-24 bg-neutral-50">
+      <Header />
     <p className="text-3xl font-semibold text-zinc-600 w-1/4">Chegou a hora de <span className="text-orange-600">otimizar</span> o cardápio da cantina.</p>
     <Image src='/logo.svg' width={200} height={200} alt=""></Image>
     </main>
+    </>
   );
 }
---
+++ web/components/header.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import { useState } from "react";
+import { User, Home, Search } from 'lucide-react'
+
+function Header() {
+    const [activeItem, setActiveItem] = useState('Home');
+
+    const handleItemClick = (item: React.SetStateAction<string>) => setActiveItem(item);
+
+    return (
+        <>
+            <header className="w-full flex justify-between fixed top-0 py-2 bg-gray-100">
+                <div className="flex flex-row space-x-20 items-center px-10">
+                    <Link href="/home">
+                        <div className="logo" onClick={() => handleItemClick('Home')}>
+                            <img src='/icon.svg' className="w-14 cursor-pointer" />
+                        </div>
+                    </Link>
+                    <nav className="flex flex-row space-x-12 items-center text-gray-600">
+                        <Link className="flex space-x-2" href="/home">
+                            <Home />
+                            <span>home</span>
+                        </Link>
+                        <Link className="flex space-x-2" href="/search">
+                            <Search />
+                            <span>search</span>
+                        </Link>
+                        <Link className="flex text-red-400" href="/about">
+                            <span>about us</span>
+                        </Link>
+                    </nav>
+                </div>
+                <div className="w-10 h-10 flex items-center justify-center text-white bg-orange-600 rounded-full m-4 mr-10">
+                    <Link href="/login">
+                        <User />
+                    </Link>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header;
